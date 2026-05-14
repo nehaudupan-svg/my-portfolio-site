a6cc4330946ac8bfd53617eee08caf3d706fbdd1
nehaudupan-svg <nehaudupan@gmail.com>
2026-05-14 12:46:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+        const toggleBtn = document.getElementById('theme-toggle');
+        
+        toggleBtn.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+            
+            if (document.body.classList.contains('dark-mode')) {
+                toggleBtn.textContent = 'Light Mode';
+            } else {
+                toggleBtn.textContent = 'Dark Mode';
+            }
+        });
